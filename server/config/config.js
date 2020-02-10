6e605c9e76b6dd8de3dcd0678e57aeea23467193
Carlos <carloscalette@gmail.com>
2020-02-10 13:37:01 -0600

    Variable de entorno MONGODB

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://unforgiven964:QaEs0TmfQWVbdwpv@cluster0-5epze.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
